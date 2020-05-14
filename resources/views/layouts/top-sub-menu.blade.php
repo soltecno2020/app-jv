a4d6d47f5aa0abca0cb706a5d48b178ca0f31546
Niko <nikoventascl@gmail.com>
2020-05-13 21:04:43 -0300

    Se modifico el sub-menu

--- resources/views/layouts/top-sub-menu.blade.php
+++ resources/views/layouts/top-sub-menu.blade.php
@@ -5,122 +5,32 @@
             <ul class="navigation-menu text-center">
 
                 <li class="has-submenu ">
-                    <a href="index.html"><i class="mdi mdi-view-dashboard"></i>Dashboard</a>
+                    <a href="index.html"><i class="mdi mdi-home"></i>Home</a>
                 </li>
                 
+                <li class="has-submenu">
+                    <a href="usuario.html"><i class="mdi mdi-account-box"></i>Usuario</a>
+                </li>    
+
+                <li class="has-submenu">
+                    <a href="vivienda.html"><i class="mdi mdi-home-variant"></i>Vivienda</a>
+                </li>
+
+                <li class="has-submenu">
+                    <a href="noticia.html"><i class="mdi mdi-newspaper"></i>Noticia</a>
+                </li>        
+
 
                 <li class="has-submenu ">
-                    <a href="calendar.html"><i class="mdi mdi-calendar-clock"></i>Calendar</a>
+                    <a href="calendar.html"><i class="mdi mdi-calendar-clock"></i>Calendario</a>
+                </li>
+
+                <li class="has-submenu ">
+                    <a href="contacto.html"><i class="mdi mdi-pencil-box"></i>Contacto</a>
+                </li>
                     
-                </li>
-
-                <li class="has-submenu">
-                    <a href="#"><i class="mdi mdi-animation"></i>User Interface</a>
-                    <ul class="submenu megamenu">
-                        <li>
-                            <ul>
-                                <li><a href="ui-badge.html">Badge</a></li>
-                                <li><a href="ui-alertify.html">Alertify</a></li>
-                                <li><a href="ui-buttons.html">Buttons</a></li>
-                                <li><a href="ui-carousel.html">Carousel</a></li>
-                                <li><a href="ui-dropdowns.html">Dropdowns</a></li>
-                                <li><a href="ui-typography.html">Typography</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <ul>
-                                <li><a href="ui-cards.html">Cards</a></li>
-                                <li><a href="ui-grid.html">Grid</a></li>
-                                <li><a href="ui-rating.html">Rating</a></li>                                            
-                                <li><a href="ui-modals.html">Modals</a></li>
-                                <li><a href="ui-images.html">Images</a></li>
-                                <li><a href="ui-tabs-accordions.html">Tabs &amp; Accordions</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <ul>
-                                <li><a href="ui-navs.html">Navs</a></li>
-                                <li><a href="ui-alerts.html">Alerts</a></li>                                                                                          
-                                <li><a href="ui-pagination.html">Pagination</a></li>
-                                <li><a href="ui-rangeslider.html">Range Slider</a></li>
-                                <li><a href="ui-progressbars.html">Progress Bars</a></li> 
-                                <li><a href="ui-popover-tooltips.html">Popover & Tooltips</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-
-                <li class="has-submenu">
-                    <a href="#"><i class="mdi mdi-cards"></i>Components</a>
-                    <ul class="submenu">
-                        <li class="has-submenu">
-                            <a href="#">Forms</a>
-                            <ul class="submenu">
-                                <li><a href="form-elements.html">Form Elements</a></li>
-                                <li><a href="form-validation.html">Form Validation</a></li>
-                                <li><a href="form-advanced.html">Form Advanced</a></li>
-                                <li><a href="form-mask.html">Form Mask</a></li>
-                                <li><a href="form-editors.html">Form Editors</a></li>
-                                <li><a href="form-uploads.html">Form File Upload</a></li>                                            
-                            </ul>
-                        </li>
-                        
-                        <li class="has-submenu">
-                            <a href="#">Charts</a>
-                            <ul class="submenu">
-                                <li><a href="charts-morris.html">Morris Chart</a></li>
-                                <li><a href="charts-chartist.html">Chartist Chart</a></li>
-                                <li><a href="charts-chartjs.html">Chartjs Chart</a></li>
-                                <li><a href="charts-flot.html">Flot Chart</a></li>
-                                <li><a href="charts-c3.html">C3 Chart</a></li>
-                                <li><a href="charts-xchart.html">X Chart</a></li>
-                                <li><a href="charts-other.html">Jquery Knob Chart</a></li>
-                            </ul>
-                        </li>
-                        <li class="has-submenu">
-                            <a href="#">Tables </a>
-                            <ul class="submenu">
-                                <li><a href="tables-basic.html">Basic Tables</a></li>
-                                <li><a href="tables-datatable.html">Data Table</a></li>
-                            </ul>
-                        </li>
-                        <li class="has-submenu">
-                            <a href="#">Icons</a>
-                            <ul class="submenu">
-                                <li><a href="icons-material.html">Material Design</a></li>
-                                <li><a href="icons-fontawesome.html">Font Awesome</a></li>
-                                <li><a href="icons-themify.html">Themify Icons</a></li>
-                            </ul>
-                        </li>
-                        <li class="has-submenu">
-                            <a href="#">Maps</a>
-                            <ul class="submenu">
-                                <li><a href="maps-google.html"> Google Map</a></li>
-                                <li><a href="maps-vector.html"> Vector Map</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-
-                <li class="has-submenu">
-                    <a href="#"><i class="mdi mdi-layers"></i>Pages</a>
-                    <ul class="submenu megamenu">
-                        <li>
-                            <ul>
-                                <li><a href="pages-login.html">Login</a></li>
-                                <li><a href="pages-register.html">Register</a></li>
-                                <li><a href="pages-recoverpw.html">Recover Password</a></li>
-                                <li><a href="pages-lock-screen.html">Lock Screen</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <ul>
-                                <li><a href="pages-blank.html">Blank Page</a></li>
-                                <li><a href="pages-404.html">Error 404</a></li>
-                                <li><a href="pages-500.html">Error 500</a></li>
-                            </ul>
-                        </li>
-                    </ul>
+                <li class="has-submenu ">
+                    <a href="cuotas.html"><i class="mdi mdi-cash"></i>Cuotas</a>
                 </li>
             </ul><!-- End navigation menu -->
         </div> <!-- end #navigation -->
