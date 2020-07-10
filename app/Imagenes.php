e0548a073b872b795c9ac0788f68bf8cd722971a
Juan Pablo Godoy <scorpion9915@hotmail.com>
2020-07-10 17:43:34 -0300

    Avance insertar imagenes

--- app/Imagenes.php
+++ app/Imagenes.php
@@ -11,4 +11,23 @@ class Imagenes extends Model
         'nombre', 'tipo', 'extension', 'portada', 'estado', 'created_at', 'updated_at', 'relacion_id' 
     ];
     protected $timestamp = true;
+
+    public static function grabarImagen($imagen,$pantalla){
+        $pantalla = "{{ route('noticias.create') }}";
+
+        if($pantalla == "{{ route('noticias.create') }}" ){
+
+            $ruta = storage_path().'/imagenes/noticias/temp/';
+
+        }else if($pantalla == "{{ route('usuarios.create') }}"){
+
+            $ruta = storage_path().'/imagenes/usuarios/temp/';
+
+        }else{
+            
+            $ruta = storage_path().'/imagenes/viviendas/temp/';
+        }
+    }
+
+
 }
--- resources/views/noticias/create.blade.php
+++ resources/views/noticias/create.blade.php
@@ -76,9 +76,41 @@
                                                         </span>
                                                     @enderror
                                                 </div>
-                                                <div class="form-group">
-                                                    <label for="imagen" class="bmd-label-floating ">Subir imagen</label>
-                                                    <input type="file" class="dropify" id="imagen" name="imagen">
+                                                <button type="button" id="nuevaImagen" class="btn btn-raised btn-primary">Agregar imagen</button> <!-- data-toggle="modal" data-target=".bd-example-modal-form"  -->
+                                                <div id="modal-agregar-imagen" class="modal fade bd-example-modal-form" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                                                    <div class="modal-dialog modal-lg">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <h5 class="modal-title" id="exampleModalform">Subir Imagen</h5>
+                                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                    <span aria-hidden="true">&times;</span>
+                                                                </button>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                <div class="form-group">
+                                                                    <input type="file" class="dropify" id="imagen" name="imagen">
+                                                                </div>
+                                                            </div>                                          
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-raised btn-danger" data-dismiss="modal">Cancelar</button>
+                                                                <button type="button" id="btn-modal-agregar-imagen" class="btn btn-raised btn-primary ml-2">Subir imagen</button>
+                                                            </div>
+                                                        </div>        
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    <table id="tablaElementos" class="table mb-0">
+                                                        <thead class="thead-default">
+                                                            <tr>
+                                                                <th>Nombre imagen</th>
+                                                                <th>Imagen</th>
+                                                                <th>Link</th>
+                                                                <th class="text-right">Acciones</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                        </tbody>
+                                                    </table>
                                                 </div>
                                                 <div class="form-group">
                                                     <h5 class="mt-0 header-title">Descripcion larga</h5>
@@ -144,5 +176,42 @@
 <!-- App js -->
 <script src="{{ asset('template/assets/js/app.js') }}"></script>
 <script>
+    $(document).ready(function(){
+        
+    });
+</script>
+<script>
+    var idElemento = 0;
+
+    $('#nuevaImagen').click(function(e){
+        e.preventDefault();
+        $('#modal-agregar-imagen').modal('show');
+    });
+
+    $('#btn-modal-agregar-imagen').click(function(e){
+        e.preventDefault();
+        subirImagen();
+    });
+
+    $('.borrar').click(function(e){
+        e.preventDefault();
+        var elemento = this.id.split('_');
+        var idElemento = elemento[1];
+        $('#tr_'+idElemento).remove();
+    });
+
+    function subirImagen(){
+        idElemento++;
+        $('#tablaElementos tbody').append(
+        '<tr id="tr_"'+idElemento+'">'+
+            '<td><input id="input_imagen_" class="form-control" type="text" disabled="on"></td>'+
+            '<td><img src=""></img></td>'+
+            '<td><button class="link btn btn-raised btn-primary ml-2" data-url=""></button></td>'+
+            '<td><div class="float-right"><div class="icon-demo-content row"><a id="bImagen_'+idElemento+'" href="" data-url=""><div class="col-sm-6 m-0"><i class="mdi mdi-close"></i></div></a></div></div></td>'+
+        '</tr>');
+        $('#modal-agregar-imagen').modal('hide');
+    }
+    
+
 </script>
 @endsection
